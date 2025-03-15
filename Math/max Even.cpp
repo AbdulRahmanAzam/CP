a9879f0afe38d01774a463f1b99ef8f5c88a1444
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2025-03-15 09:25:18 +0500

    Update max Even.cpp

--- Math/max Even.cpp
+++ Math/max Even.cpp
@@ -1,5 +1,4 @@
 // https://atcoder.jp/contests/abc272/tasks/abc272_c
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -25,7 +24,7 @@ int main(){
     ll n;
     cin >> n;
 
-    // CORRECT WAY OF DOING
+    // --------------------- CORRECT WAY OF DOING
     // ODD + ODD = EVEN
     // EVEN + EVEN = EVEN
 
@@ -58,3 +57,53 @@ int main(){
     cout << ans << "\n";
     
 
+
+
+
+// ------------------- JUGAAR NOT GOOD
+    // ll ans = -1;
+    // ll o1 = -1, o2 = -1;
+    // ll e1 = -1, e2 =-1;
+    // rep(i, 0, n){
+    //     ll x;
+    //     cin >> x;
+    //     if(x % 2 == 1)//odd
+    //         if(x > o2){
+    //             o1 = o2;
+    //             o2 = x;
+    //         }else if(x > o1)
+    //             o1 = x;
+    //     else
+    //         if(x > e2){
+    //             e1 = e2;
+    //             e2 = x;
+    //         }else if(x > e1)
+    //             e1 = x;
+    // }
+
+    // ll maxOdd = (o1 != -1 && o2 != -1) ? o1 + o2 : -1;
+    // ll maxEven = (e1 != -1 && e2 != -1) ? e1 + e2 : -1;
+
+    // ll maxOddEven = (e2 != -1 && o2 != -1) ? e2 + o2 : -1;
+
+    // maxOdd = (maxOdd % 2 == 0) ? maxOdd : -1;
+    // maxEven = (maxEven % 2 == 0) ? maxEven : -1;
+    // maxOddEven = (maxOddEven % 2 == 0) ? maxOddEven : -1;
+
+    // if(maxOdd != -1 && maxEven != -1){
+    //     ans = max(maxOdd, maxEven);
+    // }else if(maxOdd != -1 && maxOddEven != 1){
+    //     ans = max(maxOdd, maxOddEven);
+    // }else if(maxOddEven != -1 && maxEven != 1){
+    //     ans = max(maxOddEven, maxEven);
+    // }else if(maxEven != -1){
+    //     ans = maxEven;
+    // }else if(maxOddEven != -1){
+    //     ans = maxOddEven;
+
+    // }else if(maxOdd != -1){
+    //     ans = maxOdd;
+
+    // }
+    // cout << ans << endl;
+}
